3cf2603dfff7ac41df144a21b6898984fdb95ddb
Andrea-Pinales <95321339+Andrea-Pinales@users.noreply.github.com>
2022-02-16 14:08:14 -0800

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Andrea Pinales and it is
+        <a
+          href="https://github.com/Andrea-Pinales/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
