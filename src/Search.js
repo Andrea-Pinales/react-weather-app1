52a1aa2dcaa5ad82744984d6bbd69840039065eb
Andrea-Pinales <95321339+Andrea-Pinales@users.noreply.github.com>
2022-02-21 17:52:27 -0800

    Tried to start Search Engine functionality

--- src/Search.js
+++ src/Search.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 
-export default function Search() {
+export default function Search(props) {
+  const [city, setCity] = useState(props.defaultCity);
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="Search">
       <div className="row search-form">
         <div className="col-sm-6 column">
           <i className="fas fa-search"></i>
-          <input type="search" className="Input" placeholder="Enter a city" />
+          <input
+            type="search"
+            className="Input"
+            placeholder="Enter a city"
+            onChange={handleCityChange}
+          />
         </div>
 
         <div className="col column">
