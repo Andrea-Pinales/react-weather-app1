f988f20dccbad9ae6be73ff6378e7325b553660e
Andrea-Pinales <95321339+Andrea-Pinales@users.noreply.github.com>
2022-02-23 15:45:12 -0800

    Rearranged search form

--- src/Search.js
+++ src/Search.js
@@ -5,28 +5,21 @@ export default function Search(props) {
   return (
     <div className="Search">
       <div className="row search-form">
-        <div className="col-sm-6 column">
-          <i className="fas fa-search"></i>
-          <input type="search" className="Input" placeholder="Enter a city" />
-        </div>
-
-        <div className="col column">
-          <select className="Degrees">
-            <option value="celsius" className="celsius">
-              Celsius
-            </option>
-            <option value="fahrenheit" className="fahrenheit">
-              Fahrenheit
-            </option>
-          </select>
-        </div>
-
-        <div className="col column">
-          <i className="fas fa-location-arrow"></i>
-          <button type="submit" className="Current-location">
-            Current
-          </button>
-        </div>
+        <form>
+          <div className="col-6 column">
+            {/* <i className="fas fa-search"></i> */}
+            <input type="search" className="Input" placeholder="Enter a city" />
+          </div>
+          <div className="col-3 column">
+            <input type="submit" value="Search" className="SearchButton" />
+          </div>
+          <div className="col-3 column">
+            {/* <i className="fas fa-location-arrow"></i> */}
+            <button type="submit" className="CurrentLocation">
+              Current
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
